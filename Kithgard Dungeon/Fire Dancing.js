cb7711add5d7dc1850096389bd60380ef7b71d02
c3rb1n <ilya_prudnikov97@mail.ru>
2020-07-27 13:10:37 +0300

    fix commit

--- Kithgard Dungeon/Fire Dancing.js
+++
@@ -1,9 +0,0 @@
-// Code normally executes in the order it's written.
-// Loops repeat a block of code multiple times.
-
-while(true) {
-    hero.moveRight();
-    
-    // Add a moveLeft command to the loop here
-    hero.moveLeft();
-}
