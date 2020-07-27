797f0bc145da390edd89bf15adaa767b54d0d432
c3rb1n <ilya_prudnikov97@mail.ru>
2020-07-27 13:07:30 +0300

    Revert "fix commit"
    This reverts commit 84adeb495edd644b50bc4446693006c81ec7fa80.

--- Kithgard Dungeon/Enemy Mine.js
+++
@@ -1,7 +0,0 @@
-// Use arguments with move statements to move farther.
-
-hero.moveRight(3);
-hero.moveUp();
-hero.moveRight();
-hero.moveDown(3);
-hero.moveRight(2);
--- Kithgard Dungeon/Shadow Guard.js
+++
@@ -1,7 +0,0 @@
-// Avoid being seen by the ogre. Collect all the gems.
-
-hero.moveRight();
-hero.moveUp();
-hero.moveRight();
-hero.moveDown();
-hero.moveRight();
