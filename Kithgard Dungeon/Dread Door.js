0cda6c6835f754cf87c5181883b5b1b5314a946c
c3rb1n <ilya_prudnikov97@mail.ru>
2020-07-27 13:09:11 +0300

    Revert "Revert "Revert "fix commit"""
    This reverts commit 8f06bd3e5d6ae32c336b3ded31a83ff9d951099b.

--- Kithgard Dungeon/Dread Door.js
+++
@@ -1,6 +0,0 @@
-// Attack the door!
-// It will take many hits, so use a "while-true" loop.
-
-while(true) {
-    hero.attack("Door");
-}
--- Kithgard Dungeon/Gems in the Deep.js
+++
@@ -1,6 +0,0 @@
-// Grab all the gems using your movement commands.
-
-hero.moveRight();
-hero.moveDown();
-hero.moveUp(2);
-hero.moveRight();
