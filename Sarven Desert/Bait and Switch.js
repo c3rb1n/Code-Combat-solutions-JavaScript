4f706b8e6e564732e3b004bce7cd54ada148b01e
c3rb1n <ilya_prudnikov97@mail.ru>
2020-07-29 09:17:02 +0300

    add solutions for 10 levels in Sarven Desert campaign

---
+++ Sarven Desert/Bait and Switch.js
@@ -0,0 +1,31 @@
+// Lure ogres into traps with decoys.
+
+// This function has the hero collect gold until they have enoughGold.
+function collectUntil(enoughGold) {
+    // While hero.gold is less than enoughGold:
+    while(hero.gold < enoughGold) {
+        // Find a coin and take it: 
+        var coin = hero.findNearestItem();
+        
+        if (coin) {
+            hero.moveXY(coin.pos.x, coin.pos.y);
+        }
+    }      
+}
+
+// Collect 25 gold for one decoy and build it on the red mark.
+collectUntil(25);
+
+hero.buildXY("decoy", 40, 52);
+
+// It's better to hide.
+hero.moveXY(20, 52);
+
+// Use the collectUntil function to collect 50 gold:
+collectUntil(50);
+
+// Build a "decoy" on the bone mark:
+hero.buildXY("decoy", 68, 22);
+
+// Build a "decoy" on the wooden mark:
+hero.buildXY("decoy", 30, 20);
