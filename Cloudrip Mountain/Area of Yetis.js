184d75883e756f73a0bc8e0b97ccedea54fe6534
c3rb1n <ilya_prudnikov97@mail.ru>
2020-08-17 20:57:01 +0300

    add solutions for 5 levels in Cloudrip Mountain campaign

---
+++ Cloudrip Mountain/Area of Yetis.js
@@ -0,0 +1,24 @@
+// Defeat Yetis.
+
+// The chosen one can stun yetis with a Shout.
+var chosen = hero.findFriends()[0];
+
+// The power of the Shout is equal to the area of a circle.
+var radius = chosen.distanceTo(chosen.findNearestEnemy());
+
+// The area of a circle can be calculated with the formula:
+var area = radius * radius * Math.PI;
+
+// Tell the area to the chosen one.
+hero.say(area);
+
+// Don't just stand there! Fight!
+while(true) {
+    var enemy = hero.findNearestEnemy();
+    
+    if (enemy) {
+        while(enemy.health > 0) {
+            hero.attack(enemy);
+        }
+    }
+}
